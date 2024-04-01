4d81c87ca49da5ea14af55ebf0727bc5b1a52cec
Polina Sheviakova <polina.sheviakova@virtualfort.ru>
2024-04-01 21:03:58 +0300

    Add Connect method policy

--- migrations/20240314093203_migrations_create_policies_table.sql
+++ migrations/20240314093203_migrations_create_policies_table.sql
@@ -9,7 +9,8 @@ CREATE TABLE policies (
 INSERT INTO policies(endpoint, allowed_roles) VALUES
     ('/chat_v1.ChatV1/Create', ARRAY ['ADMIN']::role[]),
     ('/chat_v1.ChatV1/Delete', ARRAY ['ADMIN']::role[]),
-    ('/chat_v1.ChatV1/SendMessage', ARRAY ['ADMIN', 'USER']::role[]);
+    ('/chat_v1.ChatV1/SendMessage', ARRAY ['ADMIN', 'USER']::role[]),
+    ('/chat_v1.ChatV1/Connect', ARRAY ['ADMIN', 'USER']::role[]);
 -- +goose StatementEnd
 
 -- +goose Down
